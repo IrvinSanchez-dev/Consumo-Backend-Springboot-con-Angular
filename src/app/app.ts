6e86a299ba58f082b68a401bf31b83843a7abede
irvinjs22620075 <22620075@tlaxiaco.tecnm.mx>
2026-04-17 11:57:14 -0600

    Creacion de proyecto en Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Consumo-Backend-Angular');
+  protected readonly title = signal('Frontend With ANGULAR');
 }
